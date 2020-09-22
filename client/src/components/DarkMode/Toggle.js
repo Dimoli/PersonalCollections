4c0a7e8d02a2cd1eb5ab5046eafe759f2b7561b5
Dimoli <olifdmni@gmail.com>
2020-09-23 00:08:12 +0300

    Setting server

--- client/src/components/DarkMode/Toggle.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import DarkModeToggle from "react-dark-mode-toggle";
-
-export default (props) => (
-  <DarkModeToggle onChange={props.onChange} checked={props.checked} size={80} />
-);
--- client/src/localLang/vocabularies.js
+++
@@ -1,4 +0,0 @@
-export default {
-  Russian: { main: { navSearch: "Искать айтемы", logout: "Выйти" } },
-  English: { main: { navSearch: "Search items", logout: "Log out" } },
-};
