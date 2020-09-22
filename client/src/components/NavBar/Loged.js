ff48d381dbca159d284a752d7a98bcda1a616ef5
Dimoli <olifdmni@gmail.com>
2020-09-23 00:05:17 +0300

    Replace react-intl to useContext API

--- client/src/components/NavBar/Loged.js
+++ client/src/components/NavBar/Loged.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 
+import authContext from "../../context/auth";
+
 export default (props) => {
+  const { localLang } = useContext(authContext);
+
   return (
     <div className="log">
       <NavLink to="/personal-cabinet">
         <i className="fa fa-user-circle-o text-warning" aria-hidden="true" />
       </NavLink>
       <Button variant="danger" onClick={props.logout}>
-        Log out
+        {localLang.vocabulary.main.logout}
       </Button>
     </div>
   );
