18fe4b7199c2ee9baa14f1bbe6e37de3e48e0b18
Dimoli <olifdmni@gmail.com>
2020-09-23 01:16:53 +0300

    Add buildspec for AWS buildCode

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ app.use(express.json({ extended: true }));
 app.use(cors());
 
 app.use(require("./routes/"));
+app.get("/", (req, res) => {
+  res.send("Welcome to the club buddy, you are officiently part of band");
+});
 
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+} */
 
 require("./mongoDB/")();
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.port || config.get("port") || 5000;
 const server = app.listen(PORT, () =>
   console.log(`App has been started on port ${PORT}...`)
 );
